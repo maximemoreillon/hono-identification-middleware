e999bc3ec1b14470b54c68ddf9ac59bdcab13e44
Maxime Moreillon <m.moreillon@gmail.com>
2024-04-22 08:57:36 +0900

    init

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { Next, Context } from "hono"
+import { HTTPException } from "hono/http-exception"
+
+type Options = {
+  url: string
+}
+
+export const middleware = (opts: Options) => async (c: Context, next: Next) => {
+  const { url } = opts
+
+  const Authorization = c.req.header("Authorization")
+
+  if (!Authorization)
+    throw new HTTPException(401, { message: "Authorization header not set" })
+
+  const headers = { Authorization }
+
+  const response = await fetch(url, { headers })
+  const user = await response.json()
+
+  c.set("user", user)
+
+  await next()
+}
---
+++ server.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono"
+import { middleware } from "."
+const app = new Hono()
+
+const { IDENTIFICATION_URL } = process.env
+
+if (!IDENTIFICATION_URL) throw "IDENTIFICATION_URL not set"
+
+const opts = { url: IDENTIFICATION_URL }
+app.use(middleware(opts))
+
+app.get("/", (c) => {
+  // TODO: fix typing error
+  const user = c.get("user")
+  return c.json(user)
+})
+
+export default app
